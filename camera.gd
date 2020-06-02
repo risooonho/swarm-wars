2e73315d1c4068c292409e34411d17363478a610
VlamFletcher <66077762+VlamFletcher@users.noreply.github.com>
2020-06-02 20:30:47 +0200

    fixed camera zoom
    still need to add camera zoom/FoV change

--- camera.gd
+++ camera.gd
@@ -2,8 +2,8 @@ extends Spatial
 
 
 # Declare member variables here. Examples:
-var vel = 10
-
+var vel
+var movement = Vector3()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,30 +16,27 @@ func _process(delta):
 	pass
 
 func cameramovement(delta):
-	var movement = Vector3()
 	
-	# LEFT
-	if Input.is_action_pressed("ui_left"):
-		movement.x -= 1
+	if Input.is_key_pressed(16777237):
+		vel = 100
+	else:
+		vel = 50
+	
+	if Input.is_key_pressed(68):# RIGHT
+		movement.x = -1
+	elif Input.is_key_pressed(65):# LEFT
+		movement.x = 1
 	else:
 		movement.x = 0
-
-	# RIGHT
-	if Input.is_action_pressed("ui_right"):
-		movement.x += 1
-	else:
-		movement.x = 0
-			
-	# UP
-	if Input.is_action_pressed("ui_up"):
-		movement.z -= 1
+		
+	
+	if Input.is_key_pressed(83):# DOWN
+		movement.z = -1
+	elif Input.is_key_pressed(87):# UP
+		movement.z = 1
 	else:
 		movement.z = 0
 		
-	# DOWN
-	if Input.is_action_pressed("ui_down"):
-		movement.z += 1
-	else:
-		movement.z = 0
+
 	
 	return movement*vel*delta
